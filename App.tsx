75c8686f77298e46a6c320c5cdfb26b5b2a4b353
gabrielezagarella <gabry-zaga20@hotmail.it>
2023-05-15 12:18:57 +0200

    add exercises 1

--- App.tsx
+++ App.tsx
@@ -1,41 +1,60 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { useState } from "react";
-import CustomInput from "./components/CustomInput";
+import { StyleSheet, View } from "react-native";
 
-const App = () => {
-  const [state, setState] = useState<string>();
-
+export default function App() {
   return (
-    <View style={styles.container}>
-      <CustomInput
-        style={{ backgroundColor: "pink", borderRadius: 10 }}
-        placeholder="placeholder 1"
-        showName={(value) => {
-          console.log("value", value);
-          setState(value);
-        }}
-      />
-      {state && <Text>Questo è lo stato salvato nel padre {state}!</Text>}
-      {/* <CustomInput placeholder="placeholder 2" /> */}
-      <StatusBar style="auto" />
-    </View>
-
+    // Esempio 1
     // <View style={styles.container}>
-    //   <View style={{backgroundColor: "red", width: "100%", height: 50, flex: 1}}/>
-    //   <View style={{backgroundColor: "pink", flex: 1,  width: "100%", height: 50}}/>
-    //   <View style={{backgroundColor: "yellow", flex: 2,  width: "100%", height: 50}}/>
+    //   {/* <Text>Open up App.tsx to start working on your app!</Text> */}
+    //   <View style={styles.box} />
+    //   <View style={styles.box} />
+    //   <View style={styles.box} />
+    //   <StatusBar style="auto" />
     // </View>
+    // Esempio 2
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.containerBoxTop}>
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+      </View>
+      <View style={styles.centainerBoxCenter}>
+        <View style={styles.box} />
+        <View style={styles.box} />
+        <View style={styles.box} />
+      </View>
+      <View style={{ alignSelf: "flex-end" }}>
+        <View style={styles.box} />
+      </View>
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
+    // alignItems: "center",
+    // flexDirection: "row",
+    // justifyContent: "space-between",
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+    margin: 20,
+  },
+  centainerBoxCenter: {
     justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  containerBoxTop: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: 20,
+  },
+  box: {
+    height: 30,
+    width: 30,
+    backgroundColor: "red",
+    margin: 10,
   },
 });
-
-export default App;

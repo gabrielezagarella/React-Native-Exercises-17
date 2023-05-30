55b0dac4931407e2098491ba0aa2c19e627d1992
gabrielezagarella <gabry-zaga20@hotmail.it>
2023-05-30 10:48:49 +0200

    feat(drawer): add custom item into drawer

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -6,7 +6,14 @@ import RootStackParams from "../models/RootStackParams";
 import DetailScreen from "../screen/DetailScreen";
 import HomeScreen from "../screen/HomeScreen";
 import ROUTES from "./routes";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  DrawerContentScrollView,
+  DrawerItem,
+  DrawerItemList,
+  createDrawerNavigator,
+} from "@react-navigation/drawer";
+import { ScrollViewProps } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
 const RootStack = createStackNavigator<RootStackParams>();
 const Drawer = createDrawerNavigator();
@@ -34,12 +41,46 @@ const NavigationProvider: React.FC = () => {
   );
 };
 
+const CustomDrawerContent = (
+  props: ScrollViewProps & {
+    children?: React.ReactNode;
+  } & React.RefAttributes<ScrollView> &
+    React.ComponentProps<typeof DrawerItemList>
+) => {
+
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
+      <DrawerItem
+        label="Esci"
+        inactiveTintColor="#9D5E7B"
+        onPress={() => undefined}
+      />
+      <DrawerItem
+        label="Elimina profilo"
+        inactiveTintColor="#9D5E7B"
+        onPress={() => undefined}
+      />
+    </DrawerContentScrollView>
+  );
+};
+
 const DrawerMenu = () => {
   return (
     <NavigationContainer
       onStateChange={(navigation) => console.log(navigation)}
     >
-      <Drawer.Navigator>
+      <Drawer.Navigator
+          screenOptions={{
+            headerTintColor: "#820251",
+            headerStyle: { backgroundColor: "#9D5E7B" },
+            headerShadowVisible: true,
+            drawerInactiveTintColor:"#9D5E7B",
+            drawerActiveTintColor: "#820251",
+            drawerActiveBackgroundColor: "#BCA5AE",
+          }}
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+      >
         <Drawer.Screen
           name={ROUTES.DrawerMenu}
           component={NavigationProvider}
